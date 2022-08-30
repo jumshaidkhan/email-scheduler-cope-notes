47dfd9b99bd9afa02f698b46b1084bc4fcc8017a
jumshaidkhan <jumshaid@thesoftaims.com>
2022-08-30 20:17:58 +0500

    Add setup to run mailer job after user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,15 @@ class User < ActiveRecord::Base
 
   has_many :recieved_messages
   has_many :messages, through: :recieved_messages
-
+  after_create :send_mail
+  
+  def send_mail
+    messages = Message.pluck(:id) - self.messages.pluck(:id)
+    SendMessageJob.perform_later(self, Message.find(messages.sample)) if messages.any?
+  end
+  
   def unsent_messages
     Message.pluck(:id) - self.messages.pluck(:id)
   end
+
 end
