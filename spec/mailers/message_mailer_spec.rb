04750dc3762d58ee9bce15885869d265853cc847
jumshaidkhan <jumshaid@thesoftaims.com>
2022-08-30 20:43:27 +0500

    Add mailer specs and preview

--- spec/mailers/message_mailer_spec.rb
+++ spec/mailers/message_mailer_spec.rb
@@ -1,5 +1,18 @@
 require "rails_helper"
 
 RSpec.describe MessageMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create :user }
+  let(:message) { create :message }
+  describe "notify" do
+    let(:mail) { MessageMailer.with(user: user, message: message).submission }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Reminder")
+      expect(mail.to).to eq([user.email])
+    end
+
+    it "renders the body" do
+      expect(mail.body).to match(message.content)
+    end
+  end
 end
--- spec/mailers/previews/message_mailer_preview.rb
+++ spec/mailers/previews/message_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/message_mailer
 class MessageMailerPreview < ActionMailer::Preview
-
+  def submission
+    MessageMailer.with(user: User.first, message: Message.first).submission
+  end
 end
