41ec64d76b174983e9005cae11845ad40ebaefa6
jumshaidkhan <jumshaid@thesoftaims.com>
2022-08-30 20:43:15 +0500

    Add job specs

--- spec/jobs/send_message_job_spec.rb
+++ spec/jobs/send_message_job_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe SendMessageJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create :user }
+  let(:message) { create :message }
+  
+  describe "send message" do
+    it 'sends the message and enque another message' do
+      ActiveJob::Base.queue_adapter = :test
+      expect {
+        SendMessageJob.perform_later(user, message)
+      }.to have_enqueued_job(SendMessageJob)
+    end
+  end
+
 end
